1e3dd4091e10725b5e87d2d48f5749d6c401021f
Joy Phoebe <joyapisi@gmail.com>
2023-11-25 11:47:04 +0300

    Wrap column with dropabble

--- src/components/Column.js
+++ src/components/Column.js
@@ -3,7 +3,18 @@ function Column({column}){
     return(
         <div className="column">
         <h3>{column.title}</h3>
-   
+        <Droppable droppableId={column.id}>
+          {(provided) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              className="card-container"
+            >
+              
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
       </div> 
    )
 }
