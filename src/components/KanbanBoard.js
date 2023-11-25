a6079a5441b69651663ed4dadb737a3b6c9338fd
Joy Phoebe <joyapisi@gmail.com>
2023-11-25 17:36:55 +0300

    Use react hooks to handle changing states

--- src/components/KanbanBoard.js
+++ src/components/KanbanBoard.js
@@ -2,9 +2,23 @@ import React from "react";
 import { DragDropContext } from "react-beautiful-dnd";
 import Column from "./Column";
 import { Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import { addColumn, moveCard } from "../redux/actions";
 
 export default function kanbanBoard(){
+  const dispatch = useDispatch();
+  const columns = useSelector((state) => state.columns);
+
+  const handleDragEnd = (result) => {
+    const { source, destination, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    dispatch(moveCard(source.droppableId, destination.droppableId, draggableId, source.index, destination.index));
+  };
+
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
       <div className="kanban-board">
