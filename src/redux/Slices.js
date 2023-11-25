a4f8bb372c054e82bbb11c2dc61222eaa32bc7b5
Joy Phoebe <joyapisi@gmail.com>
2023-11-25 10:16:31 +0300

    Set up slice for the kanban board actions

---
+++ src/redux/Slices.js
@@ -0,0 +1,25 @@
+// I will import the constants for actions here
+import {
+    ADD_COLUMN,
+    ADD_CARD,
+    DELETE_COLUMN,
+    DELETE_CARD,
+    EDIT_COLUMN,
+    EDIT_CARD,
+  } from "../components/Actions";
+
+//set up initial state of column  as an empty array
+  const initialState = {
+    columns: [],
+  };
+
+//use redux reducer to set up and define state
+  const kanbanReducer = (state = initialState, action) => {
+    switch (action.type) {
+      // I will handle all the actions here
+      default:
+        return state;
+    }
+  };
+  
+  export default kanbanReducer;
