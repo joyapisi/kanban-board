621d84fe8f961f2aadef421f3cc7749efcd3d170
Joy Phoebe <joyapisi@gmail.com>
2023-11-27 17:03:21 +0300

    Update syntax for column component

--- src/components/Column.js
+++ src/components/Column.js
@@ -2,20 +2,20 @@
 import { Droppable } from 'react-beautiful-dnd';
 import Card from './Card';
 
-export default function Column({column}){
-    return(
-        <div className="column">
-        <h3>{column.title}</h3>
-        <Droppable droppableId={column.id}>
-          {(provided) => (
+export default function Column({ column }) {
+  return (
+    <div className="column">
+      <h3>{column.title}</h3>
+      <Droppable droppableId={column.id}>
+        {(provided) => (
           <div ref={provided.innerRef} {...provided.droppableProps}>
             {column.cards && column.cards.map((card, index) => (
-              <Card key={card.id} card={card} index={index} />
+                <Card key={card.id} card={card} index={index} />
             ))}
             {provided.placeholder}
           </div>
         )}
       </Droppable>
-      </div> 
-   )
+    </div>
+  );
 }
