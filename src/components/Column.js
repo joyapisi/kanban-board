7270a440feba3db0f701db8b54a016e397830432
Joy Phoebe <joyapisi@gmail.com>
2023-11-29 12:03:39 +0300

    🐛 Fix bug on how columns are added

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,16 +1,23 @@
 //This component will hold all components and the icon which adds a column
 import { Droppable } from 'react-beautiful-dnd';
 import Card from './Card';
+import Button from '@mui/material/Button';
 import AddCardModal from './modals/AddCardModal';
 import { useState } from 'react';
-export default function Column({ column }) {
+import { addCard } from './actions/Actions';
+import { useDispatch } from 'react-redux';
 
- 
+export default function Column({ column }) {
+  const dispatch = useDispatch();  
+  
   //set the state of the modal popup
   const [isAddCardModalOpen, setAddCardModalOpen] = useState(false);
+  
   //handle card addition within the column
   const handleAddCard = (cardTitle) => {
-    console.log(`Adding card: ${cardTitle}`);
+    dispatch(addCard(column.id, cardTitle));
+    setAddCardModalOpen(false);
+    // console.log(`Adding card: ${cardTitle}`);
   };
 
   return (
@@ -18,7 +25,9 @@ export default function Column({ column }) {
       <h3>{column.title}</h3>
       <Droppable droppableId={column.id}>
         {(provided) => (
-          <div ref={provided.innerRef} {...provided.droppableProps}>
+          <div 
+          className='column-details'
+          ref={provided.innerRef} {...provided.droppableProps}>
             {column.cards && column.cards.map((card, index) => (
                 <Card key={card.id} card={card} index={index} />
             ))}
@@ -26,8 +35,9 @@ export default function Column({ column }) {
           </div>
         )}
       </Droppable>
+
       {/* Button that will trigger the opening of the Add Card modal */}
-      <button onClick={() => setAddCardModalOpen(true)}>Add Card</button>
+      <Button onClick={() => setAddCardModalOpen(true)}>Add Card</Button>
 
       {/* Control opening and closing of modal when card is being added */}
       <AddCardModal
